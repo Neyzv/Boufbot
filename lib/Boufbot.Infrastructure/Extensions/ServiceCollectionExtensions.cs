473b14461ae8ccbf43c8a96968d2f658ae1b3126
Neyzu <furyax13@gmail.com>
2025-12-02 16:19:51 +0100

    feat(db): service collection extensions

---
+++ lib/Boufbot.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Boufbot.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Boufbot.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Add services of this project to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The app service collection instance.</param>
+    /// <param name="configure">An action to configure the database usage.</param>
+    /// <returns></returns>
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder>? configure = null) =>
+        services.AddDbContextFactory<BoufbotDbContext>((sp, o) => configure?.Invoke(sp, o));
+}
