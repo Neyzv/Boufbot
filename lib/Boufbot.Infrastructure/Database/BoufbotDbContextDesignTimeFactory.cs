ea9cde36e978727949bbce84f445c77946136fd0
Neyzu <furyax13@gmail.com>
2025-12-02 16:19:11 +0100

    feat(db): design time db context

---
+++ lib/Boufbot.Infrastructure/Database/BoufbotDbContextDesignTimeFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Boufbot.Infrastructure.Database;
+
+/// <summary>
+/// Class that provides an instance of the <see cref="BoufbotDbContext"/> to execute EF Core commands.
+/// </summary>
+public sealed class BoufbotDbContextDesignTimeFactory
+    : IDesignTimeDbContextFactory<BoufbotDbContext>
+{
+    public BoufbotDbContext CreateDbContext(string[] args)
+    {
+        return new BoufbotDbContext(new DbContextOptions<BoufbotDbContext>());
+    }
+}
